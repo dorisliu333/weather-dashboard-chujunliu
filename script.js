3c3bdf8fb70018b3c45de5f14a97c09f347b60a1
DorisLiu <dorisliu113@gmail.com>
2022-07-03 19:27:27 +1000

    update

--- script.js
+++ script.js
@@ -1,30 +1,66 @@
-var currentCity = document.querySelector('#current-city');
+var currentCityEl = document.querySelector('#current-city');
 var currentTemp = document.querySelector('#current-temp');
 var currentHumidity = document.querySelector('#current-humidity');
 var currentWind = document.querySelector('#current-wind');
 var currentUvindex = document.querySelector('#current-uvindex');
-var forecastEl = document.querySelector('.forecastDisplay')
-function getWeather() {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=sydney,au&APPID=f7926986a7f8a9f6a2f7973e8afc3bbd&units=metric'
+var forecastEl = document.querySelector('.forecastDisplay');
+var inputCity = document.querySelector('#cityName');
+var cityDisplayEl = document.querySelector('.city-history-display');
+var currentCity = "Sydney";
+
+
+function getCityName(){
+    forecastEl.innerHTML="";
+    currentCity = titleCase(inputCity.value);
+    getWeather(currentCity);
+    inputCity.value="";
+}
+
+// localStorage.clear()
+function displayCityHistory(){
+    var prevCities =  JSON.parse(localStorage.getItem("cities"));
+    // console.log(prevCities.length)
+    cityDisplayEl.innerHTML='';
+    prevCities?.forEach((item) => {
+        const cityDiv = document.createElement('div');
+        const cityLink = document.createElement('a');
+        cityLink.setAttribute('href','##')
+        cityDiv.setAttribute("class", "city");
+        cityLink.innerHTML = item.city;
+        cityLink.onclick = function (){
+            forecastEl.innerHTML="";
+            getWeather(item.city);
+        }
+        cityDiv.appendChild(cityLink)
+        cityDisplayEl.appendChild(cityDiv);
+    })
+}
+
+
+function getWeather(currentCity) {
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&APPID=f7926986a7f8a9f6a2f7973e8afc3bbd&units=metric`;
     fetch(weatherUrl).then(function (response) {
         if (response.ok) {
+            storeCityName(currentCity);
             response.json().then(function (data) {
-                console.log(data);
-                displayCurrentCityWeather(data);
+                displaycurrentCityWeather(data);
+                getForecast(currentCity);
+                displayCityHistory();
             });
         } else {
             alert('Error: ' + response.statusText);
         }
     });
 };
+getWeather(currentCity);
 
 
-function getForecast(){
-    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=sydney,au&appid=f7926986a7f8a9f6a2f7973e8afc3bbd&units=metric"
+function getForecast(currentCity){
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${currentCity}&appid=f7926986a7f8a9f6a2f7973e8afc3bbd&units=metric`;
     fetch(forecastUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
+                console.log(data)
                 displayForecast(data);
             });
         } else {
@@ -32,32 +68,57 @@ function getForecast(){
         }
     });
 }
-getForecast()
 
-function displayCurrentCityWeather(data){
-    currentCity.textContent = data.name;
+function displaycurrentCityWeather(data){
+    currentCityEl.textContent = data.name;
     currentTemp.textContent = Math.round(data.main.temp)+ "℃";
     currentHumidity.textContent = data.main.humidity + "%";
     currentWind.textContent = data.wind.speed + "K/M";
 }
 
 function displayForecast(data){
-    for(var i =0; i<data.list.length; i=i+8){
+    for(var i =6; i<data.list.length; i=i+8){
         var forecastItem =  document.createElement('div');
         var forecastItemDate = document.createElement('div');
         var forecastItemTemp = document.createElement('div');
         var forecastItemIcon = document.createElement('img');
-        forecastItem.setAttribute('class','col-lg-2')
+        var forecastItemDescription = document.createElement('div');
+        forecastItem.setAttribute('id','forecastItem')
+        forecastItemDate.setAttribute('id','forecastDate')
+        forecastItemTemp.setAttribute('id','forecastTemp')
         forecastItemIcon.setAttribute('id','icon');
-        forecastItemDate.textContent = data.list[i].dt_txt;
+        forecastItemDescription.setAttribute('id','forecastDescrip');
+        var time  = data.list[i].dt_txt.split(' ')[0];
+        forecastItemDate.textContent = time;
         forecastItemTemp.textContent = Math.round(data.list[i].main.temp)+ "℃";
         forecastItemIcon.src = `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`;
+        forecastItemDescription.textContent = data.list[i].weather[0].main
         forecastItem.appendChild(forecastItemDate);
         forecastItem.appendChild(forecastItemIcon);
         forecastItem.appendChild(forecastItemTemp);
+        forecastItem.appendChild(forecastItemDescription);
         forecastEl.appendChild(forecastItem)
     }
-
 }
 
-getWeather();
+function titleCase(str) {
+    var splitStr = str.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ }
+
+
+ function storeCityName(item) { 
+    var  cityList = JSON.parse(localStorage.getItem("cities")) || [];
+    for (var i in cityList) {
+        if (cityList[i].city === item){
+            return;
+        }
+    }
+    cityList.push({ city: item })
+    localStorage.setItem("cities", JSON.stringify(cityList))
+}
+
+
